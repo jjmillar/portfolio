c8f9c5a30ec4841d207777bfcb374abc77eb52eb
Julián Millar <julianjmillar@gmail.com>
2023-10-05 09:00:14 -0300

    advance

--- app/layout.js
+++ app/layout.js
@@ -19,13 +19,32 @@ export default function RootLayout ({ children }) {
               width={100}
               alt='personal photo'
             />
-            <p>some text</p>
-            <p>another text</p>
+            <div className={styles.iconsWrapper}>
+              <a href='https://github.com/jjmillar' target='blank'>
+                <ion-icon name='logo-github' />
+              </a>
+              <a href='https://www.linkedin.com/in/juli%C3%A1n-millar-vollaire/' target='blank'>
+                <ion-icon name='logo-linkedin' />
+              </a>
+              <a href='https://www.instagram.com/muramasa_bjj/' target='blank'>
+                <ion-icon name='logo-instagram' />
+              </a>
+            </div>
             <button>asd</button>
           </div>
         </div>
         {children}
       </body>
+      <script
+        type='module'
+        src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js'
+        async
+      />
+      <script
+        nomodule
+        src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js'
+        async
+      />
     </html>
   )
 }
