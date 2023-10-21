9b87ea6bd4af4d522c2ff28400a778d008e9b355
Julián Millar <julianjmillar@gmail.com>
2023-10-21 10:17:38 -0300

    upgraded icons

--- app/layout.js
+++ app/layout.js
@@ -24,13 +24,33 @@ export default function RootLayout ({ children }) {
             />
             <div className={styles.iconsWrapper}>
               <a href='https://github.com/jjmillar' target='blank'>
-                <ion-icon name='logo-github' />
+                <Image
+                  src='/github.svg'
+                  height={100}
+                  width={100}
+                  alt='personal photo'
+                  className={styles.icon}
+                />
               </a>
-              <a href='https://www.linkedin.com/in/juli%C3%A1n-millar-vollaire/' target='blank'>
-                <ion-icon name='logo-linkedin' />
+              <a
+                href='https://www.linkedin.com/in/juli%C3%A1n-millar-vollaire/'
+                target='blank'
+              >
+                <Image
+                  src='/linkedin.svg'
+                  height={100}
+                  width={100}
+                  alt='personal photo'
+                  className={styles.icon}
+                />
               </a>
               <a href='https://www.instagram.com/muramasa_bjj/' target='blank'>
-                <ion-icon name='logo-instagram' />
+                <Image
+                  src='/instagram.svg'
+                  height={100}
+                  width={100}
+                  alt='personal photo'
+                />
               </a>
             </div>
             <button>asd</button>
@@ -39,16 +59,6 @@ export default function RootLayout ({ children }) {
         {children}
         <Navbar />
       </body>
-      <script
-        type='module'
-        src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js'
-        async
-      />
-      <script
-        noModule
-        src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js'
-        async
-      />
     </html>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,14 @@
 import styles from '@/app/page.module.scss'
+import Image from 'next/image'
 
 export default function Navbar () {
   return (
     <div className={styles.navbar}>
-      <ion-icon name='home' />
-      <ion-icon name='person' />
-      <ion-icon name='school' />
-      <ion-icon name='briefcase' />
-      <ion-icon name='mail' />
+      <Image src='/home.svg' height={100} width={100} alt='home' className={styles.icon} />
+      <Image src='/user.svg' height={100} width={100} alt='user' className={styles.icon} />
+      <Image src='/school.svg' height={100} width={100} alt='school' className={styles.icon} />
+      <Image src='/briefcase.svg' height={100} width={100} alt='briefcase' className={styles.icon} />
+      <Image src='/mail.svg' height={100} width={100} alt='mail' className={styles.icon} />
     </div>
   )
 }
