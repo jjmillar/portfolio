cfcb031d6bbdbb1f8d937e8ec4c8b231f0be6d99
Julián Millar <julianjmillar@gmail.com>
2023-10-21 23:47:26 -0300

    reformat

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
-import Image from 'next/image'
 import Navbar from '@/components/navbar'
-import Github from '@/components/icons/github'
-import Linkedin from '@/components/icons/linkedin'
-import Instagram from '@/components/icons/instagram'
+import PersonalCard from '@/components/personal-card'
 
 import './globals.scss'
-import styles from './page.module.scss'
 
 export const metadata = {
   title: 'JJ Portfolio',
@@ -16,32 +12,7 @@ export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
       <body>
-        <div className={styles.personalCard}>
-          <div className={styles.personalInfo}>
-            <Image
-              src='/seiya.jpg'
-              height={100}
-              width={100}
-              alt='personal photo'
-              className={styles.profilePic}
-            />
-            <div className={styles.iconsWrapper}>
-              <a href='https://github.com/jjmillar' target='blank'>
-                <Github />
-              </a>
-              <a
-                href='https://www.linkedin.com/in/juli%C3%A1n-millar-vollaire/'
-                target='blank'
-              >
-                <Linkedin />
-              </a>
-              <a href='https://www.instagram.com/muramasa_bjj/' target='blank'>
-                <Instagram />
-              </a>
-            </div>
-            <button>asd</button>
-          </div>
-        </div>
+        <PersonalCard />
         {children}
         <Navbar />
       </body>
---
+++ components/personal-card.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import Github from '@/components/icons/github'
+import Linkedin from '@/components/icons/linkedin'
+import Instagram from '@/components/icons/instagram'
+import styles from '@/app/page.module.scss'
+
+export default function PersonalCard () {
+  return (
+    <div className={styles.personalCard}>
+      <div className={styles.personalInfo}>
+        <Image
+          src='/seiya.jpg'
+          height={100}
+          width={100}
+          alt='personal photo'
+          className={styles.profilePic}
+        />
+        <div className={styles.iconsWrapper}>
+          <a href='https://github.com/jjmillar' target='blank'>
+            <Github />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/juli%C3%A1n-millar-vollaire/'
+            target='blank'
+          >
+            <Linkedin />
+          </a>
+          <a href='https://www.instagram.com/muramasa_bjj/' target='blank'>
+            <Instagram />
+          </a>
+        </div>
+        <button>asd</button>
+      </div>
+    </div>
+  )
+}
